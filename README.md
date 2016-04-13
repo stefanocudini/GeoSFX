e3eb740148e4c3373195d281ac8afe6b46d8d1c8
stefanocudini <stefano.cudini@gmail.com>
2016-04-13 20:00:20 +0200

    bse files... base functionalities no compress

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GeoJSON-SFX
 Self Extracting GeoJSON, a tool to build compressed GeoJSON distributed over JSONP in a compact, no dependencies Javascript file
+
+
+# Installation
+```
+$ npm install
+
+```
+
+# Run
+```
+$ cd examples
+$ node app.js
+
+```
---
+++ app.js
@@ -0,0 +1,67 @@
+
+var pkg = require('./package.json'),
+	uglify = require('uglify-js');
+	express = require('express'),
+	geobuf = require('geobuf'),
+	pbf = require('pbf'),
+	fs = require('fs'),	
+	app = express();
+
+const defOpts = {
+	port: 8080,
+	timeout: 5000,
+	jsonpCallback: 'jsonpcallback',
+	dataDir: './examples/data/',
+	dataUrl: '/data/:file',
+	decoderFile: './node_modules/geobuf/dist/geobuf.js'
+};
+
+var server = app.listen(defOpts.port, function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+server.timeout = defOpts.timeout;
+
+app.get('/', function (req, res) {
+  res.send( pkg.name +' version: '+ pkg.version );
+});
+
+app.get(defOpts.dataUrl, function(req, res) {
+
+	var buf, json,
+		file = defOpts.dataDir + req.params.file;
+
+	if ( !req.query.hasOwnProperty(defOpts.jsonpCallback) ||
+		 !fs.existsSync(file) )
+		res.status(404).send('File Not Found');
+
+	json = fs.readFileSync(file, 'utf8');
+	
+	//json = geobuf.encode(json, new pbf());
+	decoder = fs.readFileSync(defOpts.decoderFile, 'utf8');
+
+	//decoder = uglify.minify(decoder).code;
+
+	callback = req.query[ defOpts.jsonpCallback ];
+
+	//console.log(decoder)
+
+	json = '(function(){'+
+
+//TODO REFACT!!!
+		//'var geobuf = require("geobuf"); '+
+		//'	 pbf = require("pbf"); '+
+		//callback+'(geobuf.decode(new pbf("'+json+'")'
+		//'));'+
+		'var JSON = '+json+';'+
+		'\r\n'+
+
+		callback+'( JSON );'+
+
+	'}());';
+
+	console.log('requested file: '+ file);
+
+	res.header("Content-Type","application/javascript");
+	res.send( json );
+});
